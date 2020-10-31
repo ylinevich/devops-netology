a5ddb9770d8b7a4ee93054fb77f017ed557b94e9
Yuriy <y.linevich@ya.ru>
2020-10-31 22:28:19 +0300

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -1,8 +1,9 @@
-    #!/bin/bash
+#!/bin/bash
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+while [[ -n "$1" ]]; do
+    echo "Parameter #$count = $1"
     count=$(( $count + 1 ))
+    shift
 done
