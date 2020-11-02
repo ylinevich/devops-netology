2d81cb4d14d4e9a28942df67bae92f15318471af
Yuriy <y.linevich@ya.ru>
2020-11-02 21:20:24 +0300

    changed rebase (main)

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,8 +1,10 @@
-    #!/bin/bash
+#!/bin/bash
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
+
+echo "====="
